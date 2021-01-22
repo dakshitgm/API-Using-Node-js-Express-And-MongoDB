b63874de0c103747b704646a9cb119c69c03f663
Dakshit Mahadeshwar <41019458+dakshitgm@users.noreply.github.com>
2021-01-22 17:20:51 +0530

    modified crud

--- app.js
+++ app.js
@@ -1,39 +1,92 @@
-const express=require('express');
 
-const app=express();
+
+const express = require('express');
+const parser = require('body-parser');
+const mongo = require("mongoose");
+const Thing = require('./models/thing');
+const { json } = require('express');
+
+const app=express()
+
+mongo.connect(***REMOVED***)
+    .then(() => {
+        console.log("connected to mongo db");
+    }).catch(error => {
+        console.log('promblem while connecting');
+        console.error(error);
+    });
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
+app.use(parser.json());
+app.post('/api/stuff', (req, res, next) => {
+   const thing = Thing({
+       title:req.body.title,
+       description:req.body.description,
+       imageUrl:req.body.imageUrl,
+       price:req.body.price,
+       userId:req.body.userId
+   });
+
+   thing.save().then( () => {
+        res.status(201).json({
+            message:"databse saved succsessfully"
+        });
+   }).catch( error => {
+        res.status(400).json({
+            message:error
+        });
+   });
+
+});
+
+app.get('/api/stuff/:id', (req, res, next) => {
+    Thing.findOne({
+        _id:req.params.id
+    }).then( things => {
+        res.status(200).json(things);
+    }).catch(error => {
+        json.status(400).json({error:error});
+    });
+});
+
+app.put('/api/stuff/:id', (req, res, next) => {
+    const thing=new Thing({
+        _id:req.params.id,
+        title:req.body.title,
+        description:req.body.description,
+        imageUrl:req.body.imageUrl,
+        price:req.body.price,
+        userId:req.body.userId
+    });
+
+    Thing.updateOne({
+        _id:req.params.id
+    }, thing).then(() => {
+        res.status(201).json({message:'sucsessfully updated thing'});
+    }).catch( error => {
+        res.status(400).json({error:error});
+    });
+});
+
+app.delete('/api/stuff/:id', (req, res, next) => {
+    Thing.deleteOne({
+        _id:req.params.id
+    }).then( () => {
+        res.status(200).json({message:"deleted succsessfully"});
+    }).catch( error => {
+        res.status(400).json({error:error});
+    });
+});
+
 app.use('/api/stuff', (req, res, next)=>{
-    const stuff=[
-        {
-            _id: '10',
-            title: 'first one',
-            description: 'no description',
-            imageUrl: '',
-            price: 5000,
-            userId: '2g34'
-        },
-        {
-            _id: '103',
-            title: 'second',
-            description: 'no descbhkwad',
-            imageUrl: '',
-            price: 5040,
-            userId: '3fg4'
-        },
-        {
-            _id: '104',
-            title: 'third',
-            description: 'no descbhkwad',
-            imageUrl: '',
-            price: 50403,
-            userId: '3fg4'
-        }
-    ];
-    res.json(stuff);
+    Thing.find().then( things =>{
+        res.status(200).json(things);
+    }).catch( error =>{
+        res.status(400).json({error:error});
+    });
 });
 module.exports=app;
